4ccb687c482905b7691bc5d2a980c54178b61a6d
Vanderson dos S. Araújo <44899291+Vanderson10@users.noreply.github.com>
2022-03-26 17:37:17 -0300

    Update and rename main.java to basico.java

---
+++ basico.java
@@ -0,0 +1,65 @@
+//Main
+
+public class Exemplo{
+	public static void main(Strings args[]){
+		System.out.println(“diz mesmo...”);
+  }
+}
+
+//Variáveis
+//A cada classe Wrraper temos a representação de um tipo. ex. o tipo int tem a classe Integer.
+
+int contagem = 2;
+String texto = “Txun”
+int ano = 1973;
+boolean ehverdade = True;
+char primeirochar = ‘a’
+int idade = 2020-ano
+double fator = 0.6;
+
+//Funções
+visibilidade tipoRetorno nome(parâmetros){
+	return “algo” 
+}
+
+Exemplos:
+	
+public static int dobro(int valor){
+	return valor*2;
+}
+public static double sum(int a, int b){
+	return 0,5+dobro(a)+dobro(b);
+}
+public static boolean ehPar(int a){
+	return valor%2==0;
+}
+
+//Ler entrada em java (Scanner)
+
+//importação
+import java.util.Scanner;
+
+Scanner sc = new Scanner(System.in);
+
+//Ler a linha da entrada
+System.out.println(sc.nextline( ));
+
+//Pegar o inteiro da entrada
+System.out.println(sc.nextInt( ));
+
+//Criar uma variável para receber o valor
+int a = sc.nextInt( );
+
+//Execução e compilação
+//JVM → máquina virtual do java, traduz o comando para bytecode para a máquina.
+
+//compila
+javac mensagem.java
+
+//executa
+java mensagem
+
+
+
+
+
--- main.java
+++
@@ -1,5 +0,0 @@
-public class Exemplo{
-	public static void main(Strings args[]){
-		System.out.println(“diz mesmo...”);
-  }
-}
